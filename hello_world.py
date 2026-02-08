bbf1e34d2c123b63d82c44723c788fe2b87742c2
goutamjha11 <goutamiisc165@gmail.com>
2026-02-08 15:32:13 +0530

    added modification to hello_world.py for PR

--- hello_world.py
+++ hello_world.py
@@ -4,6 +4,8 @@ def main():
     print("Hello, World")
     current_time = datetime.now()
     print(f"Current time is {current_time}")
+    print(f"Modified the github actions for commenting on PR")
+
 
 if __name__ == "__main__":
     main()
