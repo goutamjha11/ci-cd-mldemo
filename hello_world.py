e64f07746151384b047491d66710cfbde6ded6e2
goutamjha11 <goutamiisc165@gmail.com>
2026-02-08 14:36:55 +0530

    added a simple hello world program

---
+++ hello_world.py
@@ -0,0 +1,9 @@
+from datetime import datetime
+
+def main():
+    print("Hello, World")
+    current_time = datetime.now()
+    print(f"Current time is {current_time}")
+
+if __name__ == "__main__":
+    main()
